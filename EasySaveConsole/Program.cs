0c27743a7f11cd0be2413dc204d6537c9c69a917
Mart1Allard2 <mart1allard@gmail.com>
2025-01-31 15:23:43 +0100

    Template interface console

--- EasySaveConsole/Program.cs
+++ EasySaveConsole/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EasySave.src
+{
+    public class Program
+    {
+        public void Start()
+
+        {
+            Console.WriteLine("\n--- EasySave Menu ---");
+            Console.WriteLine("1. Ajouter une sauvegarde");
+            Console.WriteLine("2. Supprimer une sauvegarde");
+            Console.WriteLine("3. Lister les sauvegardes");
+            Console.WriteLine("4. Exécuter une sauvegarde");
+            Console.WriteLine("5. Restaurer une sauvegarde");
+            Console.WriteLine("6. Voir les logs");
+            Console.WriteLine("7. Quitter");
+
+            Console.Write("Choix : ");
+        }
+        static void Main()
+        {
+            Program interfaces = new Program();
+            interfaces.Start();
+        }
+    }
+
+}
