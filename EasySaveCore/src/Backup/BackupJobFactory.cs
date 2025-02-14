7f77b4e69a156b29c258c8d771451d1adda82d80
Vincent Da Silva <vin--cent@hotmail.fr>
2025-02-14 07:50:06 +0100

    change gitignore ban Backup folder

---
+++ EasySaveCore/src/Backup/BackupJobFactory.cs
@@ -0,0 +1,26 @@
+namespace EasySaveCore.Backup;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Factory class responsible for creating and executing backup jobs in parallel.
+/// </summary>
+public class BackupJobFactory
+{
+    /// <summary>
+    /// Creates and executes backup jobs in parallel.
+    /// </summary>
+    /// <param name="jobs">List of backup jobs to execute.</param>
+    public void CreateBackupJobsInParallel(List<BackupJob> jobs)
+    {
+        List<Task> tasks = new List<Task>();
+
+        foreach (var job in jobs)
+        {
+            tasks.Add(Task.Run(() => job.Execute()));
+        }
+
+        Task.WaitAll(tasks.ToArray());
+    }
+}
